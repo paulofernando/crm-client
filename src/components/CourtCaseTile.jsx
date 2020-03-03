f8a70945882cb7bba792cc3f1c8d5df56cb2bbe1
Paulo Fernando <pfernando@valore.com>
2020-03-03 11:38:03 -0300

    Add and format court value

--- src/components/CourtCaseTile.jsx
+++ src/components/CourtCaseTile.jsx
@@ -3,6 +3,7 @@ import './Style.css';
 
 import ContactTileSmall from "../components/ContactTileSmall";
 import ContactTile from './ContactTile';
+import NumberFormat from 'react-number-format';
 
 class CourtCaseTile extends React.Component {
     constructor(props) {
@@ -11,15 +12,18 @@ class CourtCaseTile extends React.Component {
     render() {
         return (
             <div className="courtCaseCard">
-                <div>
-                    <h3>{this.props.courtCase.title}</h3>
+                <div>                    
+                    <div className="courtCaseValue"><NumberFormat value={this.props.courtCase.value} displayType={'text'} thousandSeparator={true} prefix={'$'} /></div>
+                    <h3>{this.props.courtCase.title}</h3>                    
                     <p>{this.props.courtCase.description}</p>
+                    Contacts:
                     <div className="cardContainer">
                         {this.props.courtCase.contacts &&
                             this.props.courtCase.contacts.map(contact => (                
                                 <ContactTileSmall key={contact.id} contact={contact} />
                             ))}
                     </div>
+                    <div>{this.props.courtCase.courtDate}</div>
                 </div>
             </div>
         );
