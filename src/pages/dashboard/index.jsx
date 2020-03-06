a84ba286ce704fb3382230d699cf1f39cab12fb1
Paulo Fernando <pfernando@valore.com>
2020-03-06 13:23:02 -0300

    Search cases and contacts from Dashboard

--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -1,55 +1,84 @@
-import React from "react";
+import React, { useState } from "react";
 import {
     FormControl,
     InputGroup,
-    Button,
-    Tooltip
+    Button
 } from "react-bootstrap";
+import { Link } from '@reach/router';
 import '../../App.css'
 
 import Header from "../../components/header";
 import DashboardButton from '../../components/dashoard-button';
 
-const Dashboard = () => {
-    return (
-        <div>
-            <Header title={"Dashboard"} />
-            <div className="dashboard">
-                <div className="dashboardGroup">
-                    <h3>Court Cases</h3>
-                    <div className="dashboardRow">
-                        <DashboardButton to={`/cases`} tooltip="List" icon="grid"/>
-                        <DashboardButton to={`/case/create`} tooltip="Create" icon="create"/>
+class Dashboard extends React.Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            caseId: "",
+            contactId: ""
+        };
+        this.textCaseId = React.createRef();
+        this.textContactId = React.createRef();
+    }
+
+    handleChangeCaseId = () => {
+        this.setState({ caseId: this.textCaseId.current.value });
+    }
+
+    handleChangeContactId = () => {
+        this.setState({ contactId: this.textContactId.current.value });
+    }
+
+    render() {
+        return (
+            <div>
+                <Header title={"Dashboard"} />
+                <div className="dashboard">
+                    <div className="dashboardGroup">
+                        <h3>Court Cases</h3>
+                        <div className="dashboardRow">
+                            <DashboardButton to={`/cases`} tooltip="List" icon="grid" />
+                            <DashboardButton to={`/case/create`} tooltip="Create" icon="create" />
+                        </div>
+                        <InputGroup className="mb-3">
+                            <FormControl
+                                type="number"
+                                placeholder="Court case id"
+                                ref={this.textCaseId}
+                                onChange={e => this.handleChangeCaseId()}
+                            />
+                            <InputGroup.Append>
+                                <Link to={`/case/${this.state.caseId}`}>
+                                    <Button variant="primary">Search</Button>
+                                </Link>
+                            </InputGroup.Append>
+                        </InputGroup>
                     </div>
-                    <InputGroup className="mb-3">
-                        <FormControl
-                            type="number"
-                            placeholder="Court case id"
-                        />
-                        <InputGroup.Append>
-                            <Button variant="primary">Search</Button>
-                        </InputGroup.Append>
-                    </InputGroup>
-                </div>
-                <div className="dashboardGroup">
-                    <h3>Contacts</h3>
-                    <div className="dashboardRow">
-                        <DashboardButton to={`/contacts`} tooltip="List" icon="grid"/>
-                        <DashboardButton to={`/contact/create`} tooltip="Create" icon="create"/>                        
+                    <div className="dashboardGroup">
+                        <h3>Contacts</h3>
+                        <div className="dashboardRow">
+                            <DashboardButton to={`/contacts`} tooltip="List" icon="grid" />
+                            <DashboardButton to={`/contact/create`} tooltip="Create" icon="create" />
+                        </div>
+                        <InputGroup className="mb-3">
+                            <FormControl
+                                type="number"
+                                placeholder="Contact id"
+                                ref={this.textContactId}
+                                onChange={e => this.handleChangeContactId()}
+                            />
+                            <InputGroup.Append>
+                                <Link to={`/contact/${this.state.contactId}`}>
+                                    <Button variant="primary">Search</Button>
+                                </Link>
+                            </InputGroup.Append>
+                        </InputGroup>
                     </div>
-                    <InputGroup className="mb-3">
-                        <FormControl
-                            type="number"
-                            placeholder="Contact id"
-                        />
-                        <InputGroup.Append>
-                            <Button variant="primary">Search</Button>
-                        </InputGroup.Append>
-                    </InputGroup>
                 </div>
             </div>
-        </div>
-    );
+        );
+    }
 };
 
 export default Dashboard;
