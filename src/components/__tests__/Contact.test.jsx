113aaa5dcba6df1c4f7daef720c44cbf0f03e9d0
Paulo Fernando <pfernando@valore.com>
2020-03-09 13:37:14 -0300

    Add apollo client tests - WIP

---
+++ src/components/__tests__/Contact.test.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { MockedProvider } from '@apollo/react-testing';
+import renderer from 'react-test-renderer';
+
+import { GET_CONTACT_DETAILS, Contact } from '../../pages/contact/Contact';
+
+const mocks = [
+  {
+    request: {
+      query: GET_CONTACT_DETAILS,
+      variables: {
+        contactId: 1,
+      },
+    },
+    result: {
+      data: {
+        contact: { 
+            firstName: "Adriano",
+            lastName: "Silva",
+            caseRole: "Judge",
+            email: "as@gmail.com",
+            courtCaseId: 1
+        },
+      },
+    },
+  },
+];
+
+it('renders case without error', () => {
+  renderer.create(
+    <MockedProvider mocks={mocks} addTypename={false}>
+      <Contact contactId="1" />
+    </MockedProvider>,
+  );
+});
