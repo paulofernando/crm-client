e27d09370d78a7ce31d867bd118b3ddd2ba1cebe
Paulo Fernando <pfernando@valore.com>
2020-03-09 10:19:23 -0300

    Tests config ready to run

--- jest.setup.js
+++ jest.setup.js
@@ -4,3 +4,9 @@ import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 configure({ adapter: new Adapter() });
+
+module.exports = {    
+    "transform": {
+        "^.+\\.jsx?$": "babel-jest"
+    }
+};
--- src/components/__tests__/ContactTile.test.jsx
+++ src/components/__tests__/ContactTile.test.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import ContactTile from '../contact/ContactTile';
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
 
 describe('ContactTile', () => {
     it('renders correctly', () => {
